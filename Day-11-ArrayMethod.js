74f545df80a5a1d58f32e9063b41b7808a605a18
ShahLucky444 <shahlucky755@gmail.com>
2026-06-17 20:28:20 +0530

    revise concept of array methods

--- Day-11-ArrayMethod.js
+++ Day-11-ArrayMethod.js
@@ -1,6 +1,6 @@
-let items = ["panipuri", "aloopuri", "kachodi", "samosa"];
+// let items = ["panipuri", "aloopuri", "kachodi", "samosa"];
 
-console.log(items.join(" , "));
+// console.log(items.join(" , "));
 
 // console.log(items.unshift("ganne ka juice")); // add to first
 
@@ -15,3 +15,40 @@ console.log(items.join(" , "));
 // items.push("vadapav"); // add to last
 
 // console.log(items.toString()); // convert array into string
+
+//date: 17/6/26 practice
+
+// let friends = ["ratan", "shivlal", "lucky"]; //map
+// let mrmap = friends.map((item) => {
+//   return "mr" + item;
+// });
+
+let nums = [10, 20, 30, 40]; // map use hota hai data changing ke liye
+
+let mapnums = nums.map((item) => {
+  return item * 3;
+});
+
+let names = ["ratan", "shivlal", "lucky"];
+let ji = names.map((item) => {
+  return item + " ji";
+});
+
+let ages = [12, 18, 25, 15, 30]; // filter data me se kuch select karne ke liye
+let adult = ages.filter((item) => {
+  return item >= 18;
+});
+
+let fname = ["ratan", "shivlal", "lucky"];
+let fil = fname.filter((item) => {
+  return item === "lucky";
+});
+
+let num = [5, 10, 15, 20, 25]; // foreach har ek item pe kaam karna
+num.forEach((item) => {
+  console.log(item);
+});
+
+let findi = num.find((item) => {
+  return item > 12;
+});
