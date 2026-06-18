9e3920102b30d7ac2d6054be240ae5d7fb53b744
ShahLucky444 <shahlucky755@gmail.com>
2026-06-18 20:20:52 +0530

    deep project concept revised

---
+++ Day-25-Project-Challenges.js
@@ -0,0 +1,35 @@
+// let head = document.getElementById("head");
+let inp = document.getElementById("inp");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+
+// btn.addEventListener("click", () => {
+//   head.innerText = inp.value;
+// });
+
+btn.addEventListener("click", () => {
+  if (inp.value === "") {
+    alert("enter text");
+    return; //Bas bhai, function yahin khatam. Aage ka code mat chalao.
+  }
+  let li = document.createElement("li");
+  let done = document.createElement("button");
+  let dlt = document.createElement("button");
+  done.innerText = "Done";
+  dlt.innerText = "Delete";
+  li.innerText = inp.value;
+  list.appendChild(li);
+  li.appendChild(done);
+  li.appendChild(dlt);
+  done.addEventListener("click", () => {
+    if (li.style.textDecoration === "line-through") {
+      li.style.textDecoration = "none";
+    } else {
+      li.style.textDecoration = "line-through";
+    }
+  });
+  dlt.addEventListener("click", () => {
+    li.remove();
+  });
+  inp.value = "";
+});
