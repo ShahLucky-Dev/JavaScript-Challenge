dad1b89be516bf59c82d2fb078c1db299540ec8b
ShahLucky444 <shahlucky755@gmail.com>
2026-06-20 19:02:23 +0530

    learn about event  delegation

---
+++ Day-27-Delegation.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("#btn");
+let list = document.querySelector("#list");
+let inp = document.querySelector("#inp");
+
+btn.addEventListener("click", () => {
+  let li = document.createElement("li");
+  list.appendChild(li);
+  inp.value;
+});
