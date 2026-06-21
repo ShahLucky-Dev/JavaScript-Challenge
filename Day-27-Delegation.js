ee11e64e8068217e3a83704a5c2a1f25e7b05695
ShahLucky444 <shahlucky755@gmail.com>
2026-06-21 10:32:14 +0530

    practice  delegation

--- Day-27-Delegation.js
+++ Day-27-Delegation.js
@@ -4,6 +4,22 @@ let inp = document.querySelector("#inp");
 
 btn.addEventListener("click", () => {
   let li = document.createElement("li");
+  let dlt = document.createElement("button");
+  let done = document.createElement("button");
+  done.innerText = "Done";
+  dlt.innerText = "Delete";
   list.appendChild(li);
-  inp.value;
+
+  li.innerText = inp.value;
+  li.appendChild(done);
+  li.appendChild(dlt);
+  inp.value = "";
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.innerText === "Delete") {
+    e.target.parentElement.remove();
+  } else if (e.target.innerText === "Done") {
+    e.target.parentElement.style.textDecoration = "line-through";
+  }
 });
