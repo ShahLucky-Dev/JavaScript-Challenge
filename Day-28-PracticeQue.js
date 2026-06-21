f9a36411594ed1909eb970f510310d07edfa1298
ShahLucky444 <shahlucky755@gmail.com>
2026-06-21 10:32:44 +0530

    practice eventlistners

---
+++ Day-28-PracticeQue.js
@@ -0,0 +1,32 @@
+let inp = document.getElementById("inp");
+let head = document.getElementById("head");
+let list = document.getElementById("list");
+
+// inp.addEventListener("input", () => {
+//   head.innerText = inp.value;
+// });
+
+// inp.addEventListener("change", () => {
+//   head.innerText = inp.value;
+// });
+
+// inp.addEventListener("keyup", (e) => {
+//   if (e.key === "Enter") {
+//     head.innerText = "submitted";
+//   }
+// });
+
+// let count = 0;
+// inp.addEventListener("keydown", (e) => {
+//   count++;
+//   head.innerText = count;
+// });
+
+inp.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    let li = document.createElement("li");
+    list.appendChild(li);
+    li.innerText = inp.value;
+    inp.value = "";
+  }
+});
