8897257fd9d35275006bb59433118a7f2fe62dae
ShahLucky444 <shahlucky755@gmail.com>
2026-06-23 20:13:19 +0530

    learn about Destructing

---
+++ Day-29-Destructring.js
@@ -0,0 +1,17 @@
+// let arr = ["html", "css", "js"]; // array destructring
+
+// let [a, b, c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+const user = {
+  name: "lucky",
+  age: 19,
+};
+
+const { name, age } = user;
+
+console.log(name);
+console.log(age);
