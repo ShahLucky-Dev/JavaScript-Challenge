01db4215800b83d0e138848a9b9b24c76e2c1a3c
ShahLucky444 <shahlucky755@gmail.com>
2026-06-24 19:35:40 +0530

    practice api

--- Day-22-Api.js
+++ Day-22-Api.js
@@ -1,14 +1,27 @@
-const url = "https://pokeapi.co/api/v2/pokemon";
-let ans = document.querySelector(".text");
-let btn = document.getElementById("btn");
+// const url = "https://pokeapi.co/api/v2/pokemon";
+// let ans = document.querySelector(".text");
+// let btn = document.getElementById("btn");
 
-const getdata = async () => {
-  console.log("getting data");
-  let response = await fetch(url);
-  console.log(response);
+// const getdata = async () => {
+//   console.log("getting data");
+//   let response = await fetch(url);
+//   console.log(response);
 
-  let pokemon = await response.json();
-  ans.innerText = pokemon.results[0].name;
-};
+//   let pokemon = await response.json();
+//   ans.innerText = pokemon.results[0].name;
+// };
 
-btn.addEventListener("click", getdata);
+// btn.addEventListener("click", getdata);
+
+let list = document.getElementById("list");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+
+  .then((data) => {
+    data.forEach((user) => {
+      let li = document.createElement("li");
+      li.innerText = user.name;
+      list.appendChild(li);
+    });
+  });
